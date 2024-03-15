c0d968952ff3d6f3111be38e592bc32ad7b748f8
Robson Dobzinski <robson.dobzinski@gmail.com>
2024-03-15 11:33:10 -0300

    Create conn.php

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+
+$mysql_user = 'USER';
+$mysql_password = 'PASSWORD';
+$mysql_database = 'DATABASE';
+$mysql_server = 'localhost';
+
+/* 
+  OCS Plugins: 
+  ---
+  wingroups -> https://github.com/PluginsOCSInventory-NG/wingroups
+  uptime -> https://github.com/PluginsOCSInventory-NG/uptime
+*/
+$modules = array('wingroups', 'uptime');
+
+?>
